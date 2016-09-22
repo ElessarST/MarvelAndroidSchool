d16706d441c3b185e1eba548ac515bae1743dfa8
Olga Telezhnikova <olga.telejnikova@dz.ru>
2016-09-22 19:10:34 +0300

    merge conflicts in package names

---
+++ app/src/main/java/ru/gdgkazan/marvel/screen/eventsList/EventsView.java
@@ -0,0 +1,20 @@
+package ru.gdgkazan.marvel.screen.eventslist;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import ru.gdgkazan.marvel.content.event.Event;
+import ru.gdgkazan.marvel.general.LoadingView;
+
+/**
+ * @author Olga Telezhnikova
+ */
+
+public interface EventsView extends LoadingView {
+
+    void showEvents(@NonNull List<Event> events);
+
+    void showError();
+}
+
