0455a794e5d4904481354a02d5d2b8d311cea036
Olga Telezhnikova <olga.telejnikova@dz.ru>
2016-09-23 11:26:43 +0300

    #7 replace RealmModel to RealmObject & fix return type in CharacterRepository & fix bug with package name (omg)

--- app/src/main/java/ru/gdgkazan/marvel/repository/CharactersRepository.java
+++ app/src/main/java/ru/gdgkazan/marvel/repository/CharactersRepository.java
@@ -4,10 +4,11 @@ package ru.gdgkazan.marvel.repository;
 import android.support.annotation.NonNull;
 
 import ru.gdgkazan.marvel.content.character.CharactersResponse;
+import ru.gdgkazan.marvel.content.character.CharactersResponseData;
 import rx.Observable;
 
 public interface CharactersRepository {
 
     @NonNull
-    Observable<CharactersResponse> characters();
+    Observable<CharactersResponseData> characters();
 }
